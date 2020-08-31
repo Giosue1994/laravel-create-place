34adceda6927996e6a62c6fb5e39a27a8b00a198
Giosue1994 <giosuelentini1994@outlook.com>
2020-08-31 17:59:14 +0200

    creazione colonn

--- database/migrations/2020_08_31_154952_update_students_table.php
+++ database/migrations/2020_08_31_154952_update_students_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStudentsTable extends Migration
+class UpdateStudentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateStudentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('students', function (Blueprint $table) {
-            $table->id();
-            $table->string('nome');
-            $table->string('cognome');
-            $table->timestamps();
+        Schema::table('students', function (Blueprint $table) {
+          $table->integer('voto');
         });
     }
 
@@ -28,6 +25,8 @@ class CreateStudentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('students');
+        Schema::table('students', function (Blueprint $table) {
+            $table->dropColumn('voto');
+        });
     }
 }
