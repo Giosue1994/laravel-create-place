4f3c9f6a2490f75751b6e3bc834327a038941803
Giosue1994 <giosuelentini1994@outlook.com>
2020-08-31 18:17:39 +0200

    aggiungere righe e stampare i risultati

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Student;
 
 class StudentController extends Controller
 {
     public function indexStudents() {
-      return view('students');
+      $students = Student::all();
+
+      return view('students', compact('students'));
     }
 }
--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.layout')
 
 @section('main_content')
-  <h1>Ciao</h1>
+  <h1>Lista studenti</h1>
+
+  <div>
+    @foreach ($students as $student)
+      <ul>
+        <li>Nome: {{ $student->nome }}</li>
+        <li>Cognome: {{ $student->cognome }}</li>
+        <li>Voto: {{ $student->voto }}</li>
+      </ul>
+    @endforeach
+  </div>
 @endsection
